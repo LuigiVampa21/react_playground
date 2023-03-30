5211b7575fc0b943ebe93669eb53022f6f441147
Nathan Teissier <nmteissier@gmail.com>
2023-03-30 10:27:57 +0200

    custom hooks

---
+++ React6/src/components/ForwardCounter.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+import useCounter from '../hooks/use-counter';
+
+import Card from './Card';
+
+const ForwardCounter = () => {
+  const counter = useCounter();
+
+
+  return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
